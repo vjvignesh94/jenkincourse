aaefeaa7646e50e68d0a86caf8520e976d5fcb45
Vignesh <Vignesh.Vijayakumar@zaplabs.com>
2021-02-08 12:11:05 +0530

    Commit

---
+++ src/main/java/com/training/jenkin/JenkinTraining.java
@@ -0,0 +1,14 @@
+package com.training.jenkin;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JenkinTraining {
+	
+	private final static Logger LOGGER = Logger.getLogger(JenkinTraining.class.getName());
+	public static void main(String args[])
+	{
+		LOGGER.log(Level.INFO,"Indiside code");
+	}
+
+}
