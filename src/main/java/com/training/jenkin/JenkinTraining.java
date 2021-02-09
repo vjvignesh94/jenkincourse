70dffb5c49a219fff4882859921c28299fa47f24
Vignesh <Vignesh.Vijayakumar@zaplabs.com>
2021-02-09 11:32:50 +0530

    hgfhhr

--- src/main/java/com/training/jenkin/JenkinTraining.java
+++ src/main/java/com/training/jenkin/JenkinTraining.java
@@ -10,5 +10,30 @@ public class JenkinTraining {
 	{
 		LOGGER.log(Level.INFO,"Indiside code");
 	}
+	
+	public static int multiplyNumber(int a, int b)
+	{
+		int c = a*b;
+		LOGGER.log(Level.INFO,"The result is "+c);
+		return c;
+	}
+	public static int multiplyNumber(int a, int b, int c)
+	{
+		int d = a*b*c;
+		LOGGER.log(Level.INFO,"The result is "+d);
+		return d;
+	}
+	public static int addNumber(int a,int b)
+	{
+		int c = a+b;
+		LOGGER.log(Level.INFO,"The result is "+c);
+		return c;
+	}
+	public static int addNumber(int a,int b,int c)
+	{
+		int d = a+b+c;
+		LOGGER.log(Level.INFO,"The result is "+d);
+		return d;
+	}
 
 }
